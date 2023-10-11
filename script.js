613e4761a5520dbd38e37c6c8f3b7c0c10e1fe54
tinafazeli <131452361+tinafazeli@users.noreply.github.com>
2023-10-11 18:04:46 +0330

    add files and folders to Git

---
+++ script.js
@@ -0,0 +1,53 @@
+// like btn 
+var likebtn= document.getElementById('like');
+var likee=document.getElementById('like2');
+function Target1(){
+    likebtn.style.display='none';
+    var likebtn2= document.getElementById('like2');
+    likebtn2.style.display='inline';
+    likebtn2.style.color='red'
+}
+function Target2(){
+    likee.style.display='none';
+    likebtn.style.display='inline';
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// buy btn 
+
+var buybtn1=document.getElementById('buybtn1');
+var buybtn2=document.getElementById('buybtn2');
+function Buy1(){
+    buybtn1.style.display='none';
+    buybtn2.style.display='inline';
+    buybtn2.style.color='red'
+}
+function Buy2(){
+    buybtn2.style.display='none';
+    buybtn1.style.display='inline';
+}
